7818a5de93cd507962abc3119528054fdb4d6420
lakhwinder <lakhwinder.chdit@gmail.com>
2024-03-18 15:45:17 +0530

    error was faced

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,14 @@
-import RootLayout from './layout.js'; // Assuming `RootLayout` is defined in `layout.js`
+import RootLayout from './layout.js'; 
+import './globals.css'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../../node_modules/slick-carousel/slick/slick.css'
+import './rra'
+import { counters } from './rra';
+import Header from './Header/header';
+import Footer from './Footer/footer';
+import Main from './Main/main';
+import { GoogleAnalytics } from '@next/third-parties/google'
+// Assuming `RootLayout` is defined in `layout.js`
 
 export default function Home() {
   return (
