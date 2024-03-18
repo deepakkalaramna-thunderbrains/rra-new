00ade68732615e9acbab1813d5b132e545d10ef1
lakhwinder <lakhwinder.chdit@gmail.com>
2024-03-18 13:49:35 +0530

    netlify.toml

--- functions/contact.js
+++ functions/contact.js
@@ -8,8 +8,10 @@ const transporter = nodemailer.createTransport({
 
 exports.handler = async (event, context) => {
     try {
+        // Parse request body
         const { name, lastName, companyName, email, telephone, message } = JSON.parse(event.body);
 
+        // Send email asynchronously
         await transporter.sendMail({
             from: 'kDeepakwebdev@gmail.com',
             to: 'kDeepakwebdev@gmail.com',
@@ -24,15 +26,21 @@ exports.handler = async (event, context) => {
             `
         });
 
+        // Log success message
         console.log('Email sent successfully!');
+
+        // Return success response
         return {
             statusCode: 200,
             body: JSON.stringify({ message: "Email sent successfully" })
         };
     } catch (error) {
+        // Log error message
         console.error('Error sending email:', error);
+
+        // Return error response
         return {
-            statusCode: 400,
+            statusCode: 500,
             body: JSON.stringify({ message: "An error occurred while sending the email" })
         };
     }
