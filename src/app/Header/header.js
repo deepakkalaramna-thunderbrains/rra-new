8a40b6798eeb0c0fc2c0d844c46714defd47c563
lakhwinder <lakhwinder.chdit@gmail.com>
2024-03-19 12:44:52 +0530

    social handles rra

--- src/app/Header/header.js
+++ src/app/Header/header.js
@@ -12,7 +12,7 @@ export default function Header() {
                 <div className="w-100 h-100 d-flex align-items-center justify-content-between">
                     <ul className="list-unstyled m-0 mx-3 d-flex align-items-center p-2 cus-social-media">
                         <li className="mb-0">
-                            <a href="#">
+                            <a href="https://www.facebook.com/people/RRA-Group-Inc/61557350933003/">
                                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(255,255,255,1)"><path d="M14 13.5H16.5L17.5 9.5H14V7.5C14 6.47062 14 5.5 16 5.5H17.5V2.1401C17.1743 2.09685 15.943 2 14.6429 2C11.9284 2 10 3.65686 10 6.69971V9.5H7V13.5H10V22H14V13.5Z"></path></svg>
                             </a>
                         </li>
@@ -22,12 +22,12 @@ export default function Header() {
                             </a>
                         </li>
                         <li className="mb-0 ms-3">
-                            <a href="#">
+                            <a href="https://www.linkedin.com/company/rragroupca/">
                                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(255,255,255,1)"><path d="M6.94048 4.99993C6.94011 5.81424 6.44608 6.54702 5.69134 6.85273C4.9366 7.15845 4.07187 6.97605 3.5049 6.39155C2.93793 5.80704 2.78195 4.93715 3.1105 4.19207C3.43906 3.44699 4.18654 2.9755 5.00048 2.99993C6.08155 3.03238 6.94097 3.91837 6.94048 4.99993ZM7.00048 8.47993H3.00048V20.9999H7.00048V8.47993ZM13.3205 8.47993H9.34048V20.9999H13.2805V14.4299C13.2805 10.7699 18.0505 10.4299 18.0505 14.4299V20.9999H22.0005V13.0699C22.0005 6.89993 14.9405 7.12993 13.2805 10.1599L13.3205 8.47993Z"></path></svg>
                             </a>
                         </li>
                         <li className="mb-0 ms-3">
-                            <a href="#">
+                            <a href="https://www.instagram.com/rragroupff/">
                                 <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(255,255,255,1)"><path d="M18.2048 2.25H21.5128L14.2858 10.51L22.7878 21.75H16.1308L10.9168 14.933L4.95084 21.75H1.64084L9.37084 12.915L1.21484 2.25H8.04084L12.7538 8.481L18.2048 2.25ZM17.0438 19.77H18.8768L7.04484 4.126H5.07784L17.0438 19.77Z"></path></svg>
                             </a>
                         </li>
