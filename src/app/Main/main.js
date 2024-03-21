570ac8ad36b1cf0b14a33fa39d07929f4c1aa3f7
lakhwinder <lakhwinder.chdit@gmail.com>
2024-03-21 16:03:50 +0530

    index page section

--- src/app/Main/main.js
+++ src/app/Main/main.js
@@ -265,16 +265,57 @@ export default function Main() {
       {/* <!-- Who We Are Section Ends -->
 
     <!-- Our Services Section Start --> */}
-      <section class="pt-5 cus-rra-group-companies">
-        <div class="row">
-          <div class="col-md-12 py-4">
-            <h2 class="m-0 text-center text-uppercase">RRA GROUP COMPANIES</h2>
-          </div>
-          <div class="col-md-12 py-4">
-            <p class="text-center">RRA Group Companies Contents Here</p>
-          </div>
+      <section class="py-5 mb-5 cus-rra-group-companies">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12 py-5">
+                    <h2 class="m-0 text-center text-uppercase">RRA GROUP COMPANIES</h2>
+                </div>
+                <div class="col-md-4">
+                    <div class="oragne-gradient p-5 h-100">
+                        <img class="d-flex align-items-center justify-content-center img-fluid contain" src="images/company-logo.png" alt=""></img>
+                        <h4 class="text-white text-uppercase my-4 pt-2">RRA Trucking Ltd</h4>
+                        <button type="submit" class="btn btn-primary btn-lg">
+                            <span class="d-flex align-items-center justify-content-center">
+                                Visit Website
+                                <svg class="ms-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" fill="white"></path>
+                                </svg>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="oragne-gradient p-5 h-100">
+                        <img class="d-flex align-items-center justify-content-center img-fluid contain" src="images/company-logo.png" alt=""></img>
+                        <h4 class="text-white text-uppercase my-4 pt-2">RRA Trucking Inc.</h4>
+                        <button type="submit" class="btn btn-primary btn-lg">
+                            <span class="d-flex align-items-center justify-content-center">
+                                Visit Website
+                                <svg class="ms-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" fill="white"></path>
+                                </svg>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="oragne-gradient p-5 h-100">
+                        <img class="d-flex align-items-center justify-content-center img-fluid contain" src="images/company-logo.png" alt=""></img>
+                        <h4 class="text-white text-uppercase my-4 pt-2">HGA Logistics Inc.</h4>
+                        <button type="submit" class="btn btn-primary btn-lg">
+                            <span class="d-flex align-items-center justify-content-center">
+                                Visit Website
+                                <svg class="ms-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                    <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z" fill="white"></path>
+                                </svg>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </section>
+    </section>
 
       {/* <!-- Clients Testimonials Section Start --> */}
       <section class="py-5 cus-client-testimonials">
