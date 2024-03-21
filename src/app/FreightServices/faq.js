b1153448165f0791fd93652d04c25050919bc724
lakhwinder <lakhwinder.chdit@gmail.com>
2024-03-21 16:33:11 +0530

    build error

--- src/app/FreightServices/faq.js
+++ src/app/FreightServices/faq.js
@@ -3,66 +3,75 @@ import jQuery from 'jquery';
 import $ from  'jquery'
 import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import "../../../node_modules/slick-carousel/slick/slick"
-$('.counter').each(function() {
-            var $this = $(this),
-            countTo = $this.attr('data-count');
-            $({ countNum: $this.text()}).animate({
-                countNum: countTo
+
+
+
+
+if (process.browser) {
+    $('.counter').each(function() {
+        var $this = $(this),
+        countTo = $this.attr('data-count');
+        $({ countNum: $this.text()}).animate({
+            countNum: countTo
+        },
+        {
+            duration: 2000,
+            easing:'linear',
+            step: function() {
+                $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+                $this.text(this.countNum);
+            }
+        });
+    });
+
+    $('.hero').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: !0,
+        cssEase: 'linear',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 8000,
+        draggable: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true
+                }
             },
             {
-                duration: 2000,
-                easing:'linear',
-                step: function() {
-                    $this.text(Math.floor(this.countNum));
-                },
-                complete: function() {
-                    $this.text(this.countNum);
+                breakpoint: 768,
+                settings: {
+                    draggable: true,
                 }
-            });
-        });
-
-	    $('.hero').slick({
-            dots: true,
-            infinite: true,
-            speed: 500,
-            fade: !0,
-            cssEase: 'linear',
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 8000,
-            draggable: true,
-            arrows: false,
-            responsive: [
-	            {
-                    breakpoint: 1024,
-                    settings: {
+            },
+            {
+                breakpoint: 600,
+                settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true
-		            }
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        draggable: true,
-		            }
-		        },
-		        {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 1,
-                        draggable: true,
-                        slidesToScroll: 1
-			        }
-		        },
-		        {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        draggable: true,
-                        slidesToScroll: 1
-		            }
-		        }
-			]
-        });
+                    draggable: true,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    draggable: true,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+}
+
+
+
