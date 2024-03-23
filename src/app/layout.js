f25b56e8bb32b0dc15ba665d2e764e439ad869c6
lakhwinder <lakhwinder.chdit@gmail.com>
2024-03-23 09:19:47 +0530

    site issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "RRA Grou",
+  title: "RRA Group",
   description: "Explore our website to learn more about our logistics services and discover how RRA Group can solve customer problems, address needs, and provide unique benefits. Thank you for choosing RRA Group. We look forward to serving you and being a part of your success story.",
   keywords: "RRA Group, Logistics, One Stop Solution, Best Logistics Services, Best Service, Containers",
 };
