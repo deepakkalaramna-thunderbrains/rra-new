22cf47da009b5292bbec4c1e84e3b452bd2295f7
dev <dev@dev.com>
2024-03-27 01:44:45 +0530

    n

--- src/app/FreightServices/page.jsx
+++ src/app/FreightServices/page.jsx
@@ -4,82 +4,12 @@ import '../../../node_modules/slick-carousel/slick/slick.css'
 import Link from 'next/link'
 import Header from "../Header/header";
 import Footer from "../Footer/footer";
-// import useMountEffect from "./faq"; // Path to your custom hook
-// import jQuery from "jquery";
-// import $ from "jquery";
-// import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle";
-// import "../../../node_modules/slick-carousel/slick/slick";
+
 
 
 
 function FreightServices() {
-    // useMountEffect(() => {
-    //     $(".counter").each(function () {
-    //       var $this = $(this),
-    //         countTo = $this.attr("data-count");
-    //       $({ countNum: $this.text() }).animate(
-    //         {
-    //           countNum: countTo,
-    //         },
-    //         {
-    //           duration: 2000,
-    //           easing: "linear",
-    //           step: function () {
-    //             $this.text(Math.floor(this.countNum));
-    //           },
-    //           complete: function () {
-    //             $this.text(this.countNum);
-    //           },
-    //         }
-    //       );
-    //     });
     
-    //     $(".hero").slick({
-    //       dots: true,
-    //       infinite: true,
-    //       speed: 500,
-    //       fade: true,
-    //       cssEase: "linear",
-    //       slidesToShow: 1,
-    //       slidesToScroll: 1,
-    //       autoplay: true,
-    //       autoplaySpeed: 8000,
-    //       draggable: true,
-    //       arrows: false,
-    //       responsive: [
-    //         {
-    //           breakpoint: 1024,
-    //           settings: {
-    //             slidesToShow: 1,
-    //             slidesToScroll: 1,
-    //             infinite: true,
-    //           },
-    //         },
-    //         {
-    //           breakpoint: 768,
-    //           settings: {
-    //             draggable: true,
-    //           },
-    //         },
-    //         {
-    //           breakpoint: 600,
-    //           settings: {
-    //             slidesToShow: 1,
-    //             draggable: true,
-    //             slidesToScroll: 1,
-    //           },
-    //         },
-    //         {
-    //           breakpoint: 480,
-    //           settings: {
-    //             slidesToShow: 1,
-    //             draggable: true,
-    //             slidesToScroll: 1,
-    //           },
-    //         },
-    //       ],
-    //     });
-    //   });
     return (
         <div>
 
