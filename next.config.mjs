7611b4c12d30f04f4625c1c9c274f58ec16d5574
dev <dev@dev.com>
2024-03-26 17:40:21 +0530

    prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 // When running build
-// const nextConfig = {
-//     reactStrictMode:true,
-//     output: 'export',
-//     distDir: 'dist',
-//     trailingSlash: true,
-//     assetPrefix:"https://www.rragroup.ca/"
-// }
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode:true,
+    output: 'export',
+    distDir: 'dist',
+    trailingSlash: true,
+    assetPrefix:"https://www.rragroup.ca/"
+}
+// const nextConfig = {}
 export default nextConfig;
