51bc8788add8da7fc96bdd59dec438c79d485f3a
dev <dev@dev.com>
2024-03-28 00:20:15 +0530

    Revert "Single layout"
    This reverts commit 1ab793fa5335a8d5746b12dcef6b191e7a9ba464.

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,26 @@ import RootLayout from './layout.js';
 import './styles/index.css'
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../../node_modules/slick-carousel/slick/slick.css'
+// import Header from './Header/header';
+import Footer from './Footer/footer';
 import Main from './Main/main.jsx';
+import { GoogleAnalytics } from '@next/third-parties/google'
+import dynamic from 'next/dynamic.js';
 
 export default function Home() {
+  const Header = dynamic(() => import("./Header/header.js"), {
+    ssr: false,
+  });
   return (
     <RootLayout>
+      <div>
+        {/* Your page content goes here */}
+        <Header/>
+        {/* header section ends here */}
         <Main/>
+        <Footer/>
+        {/* footer section ends */}
+      </div>
     </RootLayout>
   );
 }
